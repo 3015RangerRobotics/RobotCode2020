308a407a247f9411b98800b081e8f7c104d3650c
Orion Grieco <Orion.grieco@Gmail.com>
2020-01-25 13:11:06 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,13 +17,13 @@ public final class Constants {
     public static final double DRIVE_I = 0;
     public static final double DRIVE_D = 0;
     public static final double DRIVE_F = 0;
-    public static final double DRIVE_PULSES_PER_FOOT = 1;
+    public static final double DRIVE_PULSES_PER_FOOT = 14159.2386107;
     public static final double DRIVE_MAX_VELOCITY = 12;
     public static final double DRIVE_MAX_ACCELERATION = 8;
     public static final double DRIVE_MAX_MOTION_ERROR = 0.1 * DRIVE_PULSES_PER_FOOT;
     public static final int DRIVE_TIME_STEP = 10;
 
-    public static final int TURRET_MOTOR = 1;
+    public static final int TURRET_MOTOR = 5;
     public static final double TURRET_P = 2.5; // 0.3
     public static final double TURRET_I = 0; // 0
     public static final double TURRET_D = 75; // 0
@@ -35,10 +35,10 @@ public final class Constants {
     public static final double TURRET_MIN_SPEED = 0.15;
     public static final double TURRET_DEGREE_MARGIN = Math.round((1 / Constants.TURRET_DEGREES_PER_PULSE) * 0.25);
     
-    public static final int SHOOTER_MOTOR = 5;
+    public static final int SHOOTER_MOTOR = 6;
     public static final double SHOOTER_P = 0;
     public static final double SHOOTER_I = 0; // 0
     public static final double SHOOTER_D = 0; // 0
     public static final double SHOOTER_F = 0; // 0
-    public static final double SHOOTER_PULSES_PER_ROTATION = 2048;
+    public static final double SHOOTER_PULSES_PER_ROTATION = 3864.3712;
 }
