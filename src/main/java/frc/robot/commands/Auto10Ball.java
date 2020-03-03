81411f76e9032be03e93e662e583cafd496a1cd9
Delila MacLeod <dmacleod343@gmail.com>
2020-03-02 19:03:48 -0500

    stuff

--- src/main/java/frc/robot/commands/Auto10Ball.java
+++ src/main/java/frc/robot/commands/Auto10Ball.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.auto_modules.DrivePathAndHarvest;
 import frc.robot.commands.auto_modules.DriveStraightAndHarvest;
 import frc.robot.subsystems.Limelight;
@@ -11,24 +13,35 @@ public class Auto10Ball extends SequentialCommandGroup {
     public Auto10Ball() {
         super(
                 new LimelightSwitchLEDMode(Limelight.LEDMode.LED_ON),
-                new DrivePathAndHarvest("10_line_to_center", -1),
-                new CG_HarvesterOfBalls().withTimeout(0.75),
+                new HarvesterDown(),
                 new TurretTurnToTargetHold(),
                 new ShooterAutoSpeed(),
-                new DrivePathAndHarvest("10_center_to_shoot", -1),
+                new ParallelDeadlineGroup(
+                        new SequentialCommandGroup(
+                                new DriveMotionProfile("10_line_to_center"),
+//                                new WaitCommand(0.5),
+                                new DriveMotionProfile("10_center_to_shoot")
+                            ),
+                        new CG_HarvesterOfBalls()
+                        ),
                 new CG_FireZeMissiles(),
                 new TurretTurnToTargetHold(),
                 new ShooterAutoSpeed(),
                 new ParallelDeadlineGroup(
-                        new DriveMotionProfile("10_five_more"),
+                        new SequentialCommandGroup(
+                                new DriveMotionProfile("10_five_more"),
+//                                new WaitCommand(0.5),
+                                new DriveMotionProfile(-8, 12, 12)
+                                ),
                         new BallHandlerHarvest(),
                         new SequentialCommandGroup(
                                 new HarvesterSet(-0.75).withTimeout(2.85),
-                                new HarvesterUp()
-                        )
+                                new HarvesterUp(),
+                                new WaitCommand(0.5),
+                                new HarvesterDown(),
+                                new HarvesterSet(-0.75)
+                         )
                 ),
-                new CG_HarvesterOfBalls().withTimeout(1),
-                new DriveStraightAndHarvest(-8, 12, 12, -0.75),
                 new CG_FireZeMissiles(),
                 new ShooterStop(),
                 new TurretToDefaultPosition()
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -9,15 +10,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
-    private TalonSRX climberMotor;
+    private TalonFX climberMotor;
     private DoubleSolenoid latchRelease;
 
     public Climber() {
-        climberMotor = new TalonSRX(Constants.CLIMBER_MOTOR);
-        climberMotor.setInverted(true);
+        climberMotor = new TalonFX(Constants.CLIMBER_MOTOR);
+        climberMotor.setInverted(false);
         latchRelease = new DoubleSolenoid(Constants.CLIMBER_LATCH_RELEASE1, Constants.CLIMBER_LATCH_RELEASE2);
         climberMotor.configVoltageCompSaturation(12.5);
         climberMotor.enableVoltageCompensation(true);
+        climberMotor.setSensorPhase(false);
 
         closeLatch();
     }
